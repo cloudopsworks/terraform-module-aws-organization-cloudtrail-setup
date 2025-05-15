1fc26c216f33b96e4f95df0a36b05b2f28e05d12
Cristian Beraha <cristian@cloudops.works>
2025-05-15 16:27:12 -0300

    Update CloudWatch log group naming to use a local variable
    Replaced hardcoded log group names with a dynamic local variable for better flexibility and consistency. This ensures the log group names are dynamically derived based on the `cloudtrail_name` variable and reduces duplication across configurations.

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -4,9 +4,12 @@
 #            Distributed Under Apache v2.0 License
 #
 
+locals {
+  cloudwatch_log_name = "/cloudtrails/${var.settings.cloudtrail_name}"
+}
 resource "aws_cloudwatch_log_group" "cloudtrail" {
   count             = var.is_hub ? 1 : 0
-  name              = var.settings.cloudtrail_name
+  name              = local.cloudwatch_log_name
   retention_in_days = try(var.settings.cloudwatch_expiration_days, 90)
   kms_key_id        = aws_kms_key.cloudwatch[0].arn
   tags              = local.all_tags
--- iam.tf
+++ iam.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "cloudtrail_role" {
     effect  = "Allow"
     actions = ["logs:CreateLogStream"]
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:cloudtrail-catch-all:log-stream:*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.cloudwatch_log_name}:log-stream:*",
     ]
   }
 
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "cloudtrail_role" {
     effect  = "Allow"
     actions = ["logs:PutLogEvents"]
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:cloudtrail-catch-all:log-stream:*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.cloudwatch_log_name}:log-stream:*",
     ]
   }
 }
